9d24db3a2b0cc3e0481144272dbe35118188e4d5
Ruso <tanor98@mail.ru>
2022-07-21 23:47:37 +0500

    Решили две задачи на первой  лекции

---
+++ 001/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 0, b = 0;
+
+Console.Write("Ведите первое число:");
+
+string username1 = Console.ReadLine();
+
+a = int.Parse(username1);
+
+Console.Write("Введите второе число: ");
+
+string username2 = Console.ReadLine();
+
+b = int.Parse(username2);
+
+if(a == b * b)
+{
+    Console.Write("Первое число является квадратом второго.");
+}
+else 
+{
+    Console.Write("Первое число не является квадратом второго.");
+}
+
+
---
+++ Задача_2/Program.cs
@@ -0,0 +1,44 @@
+﻿int a = 0;
+
+Console.Write(" Введите число, соответвующее дню недели, от 1 до 7:  ");
+
+string username = Console.ReadLine();
+
+a = int.Parse(username);
+
+if (a == 1)
+{
+    Console.Write("Сегодня Понедельник");
+}
+if (a == 2)
+{
+    Console.Write("Сегодня Вторник");
+}
+if (a == 3)
+{
+    Console.Write("Сегодня Среда");
+}
+if(a == 4)
+{
+    Console.Write("Сегодня Четверг");
+}
+if(a == 5)
+{
+    Console.Write("Сегодня Пятница");
+}
+if(a == 6)
+{   
+    Console.Write("Сегодня Суббота");
+}
+if(a == 7)
+{
+    Console.Write("Сегодня Воскресенье");
+}
+if(a > 7)
+{
+    Console.Write("Введите число от 1 до 7!");
+}
+if(a < 1)
+{
+    Console.Write("Введите число от 1 до 7!");
+}
